adf70905f88a71bb03820a71e695ec630c8b12ee
Lukas Siatka <lukasz@lukaszsiatka.pl>
2022-03-08 06:07:49 +0100

    Chore: updates omnidirectional example - wraps it in a fixed size div, adds new nodes and edges

--- example/src/Omnidirectional/index.tsx
+++ example/src/Omnidirectional/index.tsx
@@ -14,7 +14,7 @@ import ReactFlow, {
   ArrowHeadType,
 } from 'react-flow-renderer';
 
-const onPaneScroll = (event?: MouseEvent) => console.log('pane scroll', event);
+const onPaneScroll = (event?: MouseEvent) => {};
 const onLoad = (reactFlowInstance: OnLoadParams) => {
   reactFlowInstance.fitView();
 };
@@ -26,7 +26,7 @@ const initialElements: Elements = [
     data: {
       label: 'Omnidirectional block',
       borderColor: '#000',
-      background: '#fff'
+      background: '#fff',
     },
     position: { x: 300, y: 100 },
   },
@@ -37,7 +37,7 @@ const initialElements: Elements = [
       label: 'LINK',
       hyperlink: 'https://example.org',
       borderColor: '#000',
-      background: '#fff'
+      background: '#fff',
     },
     position: { x: 300, y: 400 },
   },
@@ -49,7 +49,7 @@ const initialElements: Elements = [
       label: 'rounded node',
       borderColor: '#f0f',
       background: '#000',
-      fontColor: '#fff'
+      fontColor: '#fff',
     },
     position: { x: 100, y: 0 },
   },
@@ -58,10 +58,11 @@ const initialElements: Elements = [
     type: 'unidirectional',
     data: {
       shape: 'rounded',
-      label: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in nisi orci. Mauris faucibus nunc sagittis magna iaculis porttitor. Vestibulum sed pulvinar libero. In tincidunt cursus dolor at tristique. Praesent pellentesque rhoncus massa, a blandit magna efficitur ornare. Nam a luctus odio, at ultrices ante. Suspendisse sodales ullamcorper mi in finibus.',
+      label:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in nisi orci. Mauris faucibus nunc sagittis magna iaculis porttitor. Vestibulum sed pulvinar libero. In tincidunt cursus dolor at tristique. Praesent pellentesque rhoncus massa, a blandit magna efficitur ornare. Nam a luctus odio, at ultrices ante. Suspendisse sodales ullamcorper mi in finibus.',
       borderColor: '#f0f',
       background: '#000',
-      fontColor: '#fff'
+      fontColor: '#fff',
     },
     position: { x: 100, y: 600 },
   },
@@ -78,7 +79,7 @@ const initialElements: Elements = [
       shape: 'rounded',
       label: 'Left',
       borderColor: '#000',
-      background: '#fff'
+      background: '#fff',
     },
     position: { x: 300, y: 800 },
   },
@@ -89,9 +90,137 @@ const initialElements: Elements = [
       shape: 'rounded',
       label: 'Right',
       borderColor: '#000',
-      background: '#fff'
+      background: '#fff',
     },
     position: { x: 500, y: 800 },
+  },
+  {
+    id: '8',
+    type: 'unidirectional',
+    data: {
+      shape: 'rectangle',
+      label: 'Top',
+      borderColor: '#000',
+      background: '#fff',
+    },
+    position: { x: 300, y: 500 },
+  },
+  {
+    id: '9',
+    type: 'unidirectional',
+    data: {
+      shape: 'rectangle',
+      label: 'Bottom',
+      borderColor: '#000',
+      background: '#fff',
+    },
+    position: { x: 300, y: 598 },
+  },
+  {
+    id: '10',
+    type: 'breakpoint',
+    data: {},
+    position: { x: 600, y: 620 },
+  },
+  {
+    id: '11',
+    type: 'unidirectional',
+    data: {
+      shape: 'rounded',
+      label: 'TOP',
+      borderColor: '#000',
+      background: '#fff',
+    },
+    position: { x: 850, y: 100 },
+  },
+  {
+    id: '12',
+    type: 'unidirectional',
+    data: {
+      shape: 'rounded',
+      label: 'BOTTOM',
+      borderColor: '#000',
+      background: '#fff',
+    },
+    position: { x: 850, y: 300 },
+  },
+  {
+    id: '13',
+    type: 'unidirectional',
+    data: {
+      shape: 'rounded',
+      label: 'LEFT',
+      borderColor: '#000',
+      background: '#fff',
+    },
+    position: { x: 700, y: 200 },
+  },
+  {
+    id: '14',
+    type: 'unidirectional',
+    data: {
+      shape: 'rounded',
+      label: 'RIGHT',
+      borderColor: '#000',
+      background: '#fff',
+    },
+    position: { x: 1000, y: 200 },
+  },
+  {
+    id: '15',
+    type: 'breakpoint',
+    data: {},
+    position: { x: 926, y: 222 },
+  },
+  {
+    id: '16',
+    type: 'unidirectional',
+    data: {
+      shape: 'rounded',
+      label: 'TOP from breakpoint',
+      borderColor: '#000',
+      background: '#fff',
+    },
+    position: { x: 850, y: 400 },
+  },
+  {
+    id: '17',
+    type: 'unidirectional',
+    data: {
+      shape: 'rounded',
+      label: 'BOTTOM from breakpoint',
+      borderColor: '#000',
+      background: '#fff',
+    },
+    position: { x: 850, y: 600 },
+  },
+  {
+    id: '18',
+    type: 'unidirectional',
+    data: {
+      shape: 'rounded',
+      label: 'LEFT from breakpoint',
+      borderColor: '#000',
+      background: '#fff',
+    },
+    position: { x: 700, y: 500 },
+  },
+  {
+    id: '19',
+    type: 'unidirectional',
+    data: {
+      shape: 'rounded',
+      label: 'RIGHT from breakpoint',
+      borderColor: '#000',
+      background: '#fff',
+    },
+    position: { x: 1000, y: 500 },
+  },
+  {
+    id: '20',
+    type: 'breakpoint',
+    data: {},
+    position: { x: 926, y: 522 },
   },
   {
     id: 'e1-2',
@@ -102,7 +231,7 @@ const initialElements: Elements = [
     sourceHandle: Position.Bottom,
     type: 'smoothstep',
     labelXYOffset: { x: 0, y: -50 },
-    arrowHeadType: ArrowHeadType.DoubleArrow
+    arrowHeadType: ArrowHeadType.DoubleArrow,
   },
   {
     id: 'e1-2-2',
@@ -111,7 +240,7 @@ const initialElements: Elements = [
     targetHandle: Position.TopLeft,
     sourceHandle: Position.BottomLeft,
     type: 'smoothstep',
-    arrowHeadType: ArrowHeadType.DoubleArrow
+    arrowHeadType: ArrowHeadType.DoubleArrow,
   },
   {
     id: 'e1-2-3',
@@ -120,7 +249,7 @@ const initialElements: Elements = [
     targetHandle: Position.BottomRight,
     sourceHandle: Position.TopRight,
     type: 'smoothstep',
-    arrowHeadType: ArrowHeadType.DoubleArrow
+    arrowHeadType: ArrowHeadType.DoubleArrow,
   },
   {
     id: 'e3-4',
@@ -131,7 +260,7 @@ const initialElements: Elements = [
     sourceHandle: Position.Bottom,
     type: 'smoothstep',
     labelXYOffset: { x: 0, y: 200 },
-    arrowHeadType: ArrowHeadType.DoubleArrow
+    arrowHeadType: ArrowHeadType.DoubleArrow,
   },
   {
     id: 'e3-5',
@@ -140,7 +269,7 @@ const initialElements: Elements = [
     targetHandle: Position.Top,
     sourceHandle: Position.Bottom,
     type: 'smoothstep',
-    arrowHeadType: ArrowHeadType.DoubleArrow
+    arrowHeadType: ArrowHeadType.DoubleArrow,
   },
   {
     id: 'e5-4',
@@ -149,7 +278,7 @@ const initialElements: Elements = [
     targetHandle: Position.LeftTop,
     sourceHandle: Position.Bottom,
     type: 'smoothstep',
-    arrowHeadType: ArrowHeadType.DoubleArrow
+    arrowHeadType: ArrowHeadType.DoubleArrow,
   },
   {
     id: 'e6-7',
@@ -158,7 +287,7 @@ const initialElements: Elements = [
     targetHandle: Position.Left,
     sourceHandle: Position.Right,
     type: 'smoothstep',
-    arrowHeadType: ArrowHeadType.DoubleArrow
+    arrowHeadType: ArrowHeadType.DoubleArrow,
   },
   {
     id: 'e6-7-2',
@@ -167,7 +296,7 @@ const initialElements: Elements = [
     targetHandle: Position.LeftTop,
     sourceHandle: Position.RightTop,
     type: 'smoothstep',
-    arrowHeadType: ArrowHeadType.DoubleArrow
+    arrowHeadType: ArrowHeadType.DoubleArrow,
   },
   {
     id: 'e7-6',
@@ -176,7 +305,7 @@ const initialElements: Elements = [
     targetHandle: Position.RightBottom,
     sourceHandle: Position.LeftBottom,
     type: 'smoothstep',
-    arrowHeadType: ArrowHeadType.DoubleArrow
+    arrowHeadType: ArrowHeadType.DoubleArrow,
   },
   {
     id: 'e5-1',
@@ -185,7 +314,97 @@ const initialElements: Elements = [
     targetHandle: Position.Left,
     sourceHandle: Position.Right,
     type: 'smoothstep',
-    arrowHeadType: ArrowHeadType.DoubleArrow
+    arrowHeadType: ArrowHeadType.DoubleArrow,
+  },
+  {
+    id: 'e8-10',
+    source: '8',
+    target: '10',
+    targetHandle: Position.Top,
+    sourceHandle: Position.Right,
+    type: 'smoothstep',
+    arrowHeadType: ArrowHeadType.DoubleArrow,
+  },
+  {
+    id: 'e10-9',
+    source: '10',
+    target: '9',
+    targetHandle: Position.Right,
+    sourceHandle: Position.Left,
+    type: 'smoothstep',
+    arrowHeadType: ArrowHeadType.DoubleArrow,
+  },
+  {
+    id: 'e11-15',
+    source: '11',
+    target: '15',
+    targetHandle: Position.Top,
+    sourceHandle: Position.Bottom,
+    type: 'smoothstep',
+    arrowHeadType: ArrowHeadType.DoubleArrow,
+  },
+  {
+    id: 'e12-15',
+    source: '12',
+    target: '15',
+    targetHandle: Position.Bottom,
+    sourceHandle: Position.Top,
+    type: 'smoothstep',
+    arrowHeadType: ArrowHeadType.DoubleArrow,
+  },
+  {
+    id: 'e13-15',
+    source: '13',
+    target: '15',
+    targetHandle: Position.Left,
+    sourceHandle: Position.Right,
+    type: 'smoothstep',
+    arrowHeadType: ArrowHeadType.DoubleArrow,
+  },
+  {
+    id: 'e14-15',
+    source: '14',
+    target: '15',
+    targetHandle: Position.Right,
+    sourceHandle: Position.Left,
+    type: 'smoothstep',
+    arrowHeadType: ArrowHeadType.DoubleArrow,
+  },
+  {
+    id: 'e20-16',
+    source: '20',
+    target: '16',
+    targetHandle: Position.Bottom,
+    sourceHandle: Position.Top,
+    type: 'smoothstep',
+    arrowHeadType: ArrowHeadType.DoubleArrow,
+  },
+  {
+    id: 'e20-17',
+    source: '20',
+    target: '17',
+    targetHandle: Position.Top,
+    sourceHandle: Position.Bottom,
+    type: 'smoothstep',
+    arrowHeadType: ArrowHeadType.DoubleArrow,
+  },
+  {
+    id: 'e20-18',
+    source: '20',
+    target: '18',
+    targetHandle: Position.Right,
+    sourceHandle: Position.Left,
+    type: 'smoothstep',
+    arrowHeadType: ArrowHeadType.DoubleArrow,
+  },
+  {
+    id: 'e20-19',
+    source: '20',
+    target: '19',
+    targetHandle: Position.Left,
+    sourceHandle: Position.Right,
+    type: 'smoothstep',
+    arrowHeadType: ArrowHeadType.DoubleArrow,
   },
 ];
 
@@ -194,30 +413,43 @@ const snapGrid: SnapGrid = [1, 1];
 
 const OmnidirectionalFlow = () => {
   const [elements, setElements] = useState(initialElements);
-  const onElementsRemove =
-    (elementsToRemove: Elements) => setElements((els) => removeElements(elementsToRemove, els));
-  const onConnect =
-    (params: Connection | Edge) => setElements((els) => addEdge({...params, type: 'smoothstep'}, els));
+  const onElementsRemove = (elementsToRemove: Elements) =>
+    setElements((els) => removeElements(elementsToRemove, els));
+  const onConnect = (params: Connection | Edge) =>
+    setElements((els) =>
+      addEdge({ ...params, type: 'smoothstep', arrowHeadType: ArrowHeadType.DoubleArrow }, els)
+    );
+
+  const diagramContainerStyle = {
+    height: '1000px',
+    width: '1500px',
+    overflow: 'auto',
+    border: '1px solid #000',
+  };
 
   return (
-    <ReactFlowProvider>
-      <ReactFlow
-        connectionMode={ConnectionMode.Loose}
-        elements={elements}
-        onElementsRemove={onElementsRemove}
-        deleteKeyCode={46}
-        onConnect={onConnect}
-        onPaneScroll={onPaneScroll}
-        onLoad={onLoad}
-        connectionLineStyle={connectionLineStyle}
-        snapToGrid={true}
-        snapGrid={snapGrid}
-        paneMoveable={true}
-        minZoom={1}
-        maxZoom={4}
-        onNodeResize={(section, dimensions) => console.log(dimensions)}
-      />
-    </ReactFlowProvider>
+    <div>
+      <div style={diagramContainerStyle}>
+        <ReactFlowProvider>
+          <ReactFlow
+            connectionMode={ConnectionMode.Loose}
+            elements={elements}
+            onElementsRemove={onElementsRemove}
+            deleteKeyCode={46}
+            onConnect={onConnect}
+            onPaneScroll={onPaneScroll}
+            onLoad={onLoad}
+            connectionLineStyle={connectionLineStyle}
+            snapToGrid={true}
+            snapGrid={snapGrid}
+            paneMoveable={true}
+            minZoom={1}
+            maxZoom={4}
+            onNodeResize={(section, dimensions) => console.log(dimensions)}
+          />
+        </ReactFlowProvider>
+      </div>
+    </div>
   );
 };
 
